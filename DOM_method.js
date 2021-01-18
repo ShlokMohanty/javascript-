b0778691e58c8d5b0e7100409adc19360c744447
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-18 13:00:22 +0530

    Update DOM_method.js

--- DOM_method.js
+++ DOM_method.js
@@ -2,3 +2,6 @@ const title = document.getElementById('Page-Title');
 console.log(title);
 const errors = document.getElementsByClassName('errors');
 console.log(errors);
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
