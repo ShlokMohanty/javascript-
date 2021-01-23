fe382f487c9332289b761f7d498c2e7db58e0f7f
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-23 09:32:41 +0530

    this is the javascript changing css style file
    we had made alredy its html file

---
+++ changing_css_style.js
@@ -0,0 +1,9 @@
+const title = document.querySelector('h1');
+console.log(title.style);
+console.log(title.style.color);
+title.style.margin = '50px';
+title.style.color='crimpson';
+title.style.fontSize='60px';
+title.style.margin = '';
+
+
