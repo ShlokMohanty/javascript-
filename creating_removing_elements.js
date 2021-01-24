17044f64815ce1bda915e28b12e8b5f5c50d1fe1
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-24 23:33:52 +0530

    Create creating_removing_elements.js

---
+++ creating_removing_elements.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll('li');
+items.forEach(item => {
+item.addEventListener('click',e => {
+ // console.log(e);
+  //console.log(e.target);
+e.target.remove();
+});
+});
