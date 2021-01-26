dcd9159ed088d2bd72d3ea7919e0d7d70b74b2dd
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-26 15:24:35 +0530

    Update creating_removing_elements.js

--- creating_removing_elements.js
+++ creating_removing_elements.js
@@ -6,3 +6,16 @@ item.addEventListener('click',e => {
 e.target.remove();
 });
 });
+
+
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+button.addEventListener('click',() => {
+  ul.innerHTML += '<li>something new</li>';
+});
+const items = document.querySelectorAll('li');
+items.forEach(item =>{
+  item.addEventListener('click',e =>{
+    e.target.remove();
+  });
+});
